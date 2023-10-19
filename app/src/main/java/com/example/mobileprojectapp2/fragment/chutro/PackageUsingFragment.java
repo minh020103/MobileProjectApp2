8e042040db22011ec10671515ece70d3a179d8a2
Minh03-code <21211tt4504@mail.tdc.edu.vn>
2023-10-20 01:01:34 +0700

    hoàn thành thanh điều hướng

--- app/src/main/java/com/example/mobileprojectapp2/fragment/chutro/PackageUsingFragment.java
+++ app/src/main/java/com/example/mobileprojectapp2/fragment/chutro/PackageUsingFragment.java
@@ -1,2 +1,22 @@
-package com.example.mobileprojectapp2.fragment.chutro;public class PackageUsingFragment {
+package com.example.mobileprojectapp2.fragment.chutro;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.mobileprojectapp2.R;
+
+public class PackageUsingFragment extends AbstractFragment{
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View fragmentLayout = null;
+        fragmentLayout = inflater.inflate(R.layout.chutro_fragment_package_using_layout, container, false);
+
+        return fragmentLayout;
+    }
 }
