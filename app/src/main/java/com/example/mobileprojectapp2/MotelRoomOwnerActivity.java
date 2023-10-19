f6b9f79e46ec2f370d97b7ec2266385adc085cbb
Minh03-code <21211tt4504@mail.tdc.edu.vn>
2023-10-19 21:43:38 +0700

    Initial commit

---
+++ app/src/main/java/com/example/mobileprojectapp2/MotelRoomOwnerActivity.java
@@ -0,0 +1,13 @@
+package com.example.mobileprojectapp2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MotelRoomOwnerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.motel_room_owner_layout);
+    }
+}
