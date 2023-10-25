ba11de56a8e8497945a6f887807aacb86f11c9b9
Minh03-code <21211tt4504@mail.tdc.edu.vn>
2023-10-25 08:37:41 +0700

    update

---
+++ app/src/main/java/com/example/mobileprojectapp2/activity/chutro/EditRoomActivity.java
@@ -0,0 +1,14 @@
+package com.example.mobileprojectapp2.activity.chutro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditRoomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.chutro_edit_room_layout);
+    }
+}
