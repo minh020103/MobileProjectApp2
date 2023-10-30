1b073f684b68f955047b5f0ebfea3723ac7355bb
KietDo1 <21211tt2801@mail.tdc.edu.vn>
2023-10-30 07:39:20 +0700

    commit subnet

---
+++ app/src/main/java/com/example/mobileprojectapp2/activity/chutro/NotificationDetailActivity.java
@@ -0,0 +1,14 @@
+package com.example.mobileprojectapp2.activity.chutro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NotificationDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification_detail);
+    }
+}
---
+++ app/src/main/java/com/example/mobileprojectapp2/activity/chutro/RenterDetailActivity.java
@@ -0,0 +1,14 @@
+package com.example.mobileprojectapp2.activity.chutro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RenterDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_renter_detail);
+    }
+}
