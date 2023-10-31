92144c70500f767b16b1f25f63fa5c8bd215b318
Minh03-code <21211tt4504@mail.tdc.edu.vn>
2023-10-31 14:36:12 +0700

    update

--- app/src/main/java/com/example/mobileprojectapp2/activity/chutro/AddRoomActivity.java
+++ app/src/main/java/com/example/mobileprojectapp2/activity/chutro/AddRoomActivity.java
@@ -1,26 +1,97 @@
 package com.example.mobileprojectapp2.activity.chutro;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
 import com.example.mobileprojectapp2.R;
 
+import java.io.IOException;
+
 public class AddRoomActivity extends AppCompatActivity {
 
     private ImageView imgBack;
+    private RecyclerView rcvChoosedImages;
+    private final int RQ = 10001;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.chutro_add_room_layout);
         imgBack = findViewById(R.id.imgBack);
+        rcvChoosedImages = findViewById(R.id.rcvChoosedImages);
+
+
+
+        batSuKien();
+    }
+
+    private void batSuKien() {
         imgBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 finish();
             }
         });
+        rcvChoosedImages.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkPesmission();
+            }
+        });
     }
+
+    private void checkPesmission(){
+        if(Build.VERSION.SDK_INT<Build.VERSION_CODES.M){
+            openGallery();
+            return;
+        }
+        if(checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)== PackageManager.PERMISSION_GRANTED){
+            openGallery();
+        }else{
+            String[] permission = {Manifest.permission.READ_EXTERNAL_STORAGE};
+            requestPermissions(permission,RQ);
+        }
+    }
+    private void openGallery() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        mActivityResultLauncher.launch(Intent.createChooser(intent,"Select Picture"));
+    }
+    private ActivityResultLauncher<Intent> mActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if(result.getResultCode() == Activity.RESULT_OK){
+                        Intent data = result.getData();
+                        if(data==null){
+                            return;
+                        }
+                        Uri uri = data.getData();
+                        try{
+                            Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),uri);
+                        }catch (IOException e){
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+    );
 }
