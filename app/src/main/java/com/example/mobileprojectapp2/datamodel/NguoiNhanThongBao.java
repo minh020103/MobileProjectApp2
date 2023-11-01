9d06ba39eb14455c1e581eeb81afa37dd990c84e
KietDo1 <21211tt2801@mail.tdc.edu.vn>
2023-11-01 08:29:14 +0700

    commit subnet

---
+++ app/src/main/java/com/example/mobileprojectapp2/datamodel/NguoiNhanThongBao.java
@@ -0,0 +1,35 @@
+package com.example.mobileprojectapp2.datamodel;
+
+public class NguoiGuiThongBao {
+    private int id;
+    private int loaiTaiKhoan;
+
+    public NguoiGuiThongBao(int id, int loaiTaiKhoan) {
+        this.id = id;
+        this.loaiTaiKhoan = loaiTaiKhoan;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getLoaiTaiKhoan() {
+        return loaiTaiKhoan;
+    }
+
+    public void setLoaiTaiKhoan(int loaiTaiKhoan) {
+        this.loaiTaiKhoan = loaiTaiKhoan;
+    }
+
+    @Override
+    public String toString() {
+        return "NguoiGuiThongBao{" +
+                "id=" + id +
+                ", loaiTaiKhoan=" + loaiTaiKhoan +
+                '}';
+    }
+}
