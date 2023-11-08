b8a60003e4c5f88cdd2a9969a47b739b2bebcc9c
KietDo1 <21211tt2801@mail.tdc.edu.vn>
2023-11-08 10:08:59 +0700

    do luc debug chung minh lam tren master nen tui commit tren master nhe

--- app/src/main/java/com/example/mobileprojectapp2/api/Const.java
+++ app/src/main/java/com/example/mobileprojectapp2/api/Const.java
@@ -1,7 +1,7 @@
 package com.example.mobileprojectapp2.api;
 
 public class Const {
-    public static String DOMAIN = "http://192.168.190.1/3t/laravel/public/";
+    public static String DOMAIN = "http://192.168.1.11/3t/laravel/public/";
     public static int senderId = 2;
     public static final String PRE_LOGIN = "SharedPreferencesLogin";
 
