baeb418693efaa6ab7866732afc12b08a7164496
KietDo1 <21211tt2801@mail.tdc.edu.vn>
2023-11-27 17:46:41 +0700

    commit subnet

---
+++ app/src/main/java/com/example/mobileprojectapp2/activity/chutro/YeuCauDatPhongChiTietActivity.java
@@ -0,0 +1,14 @@
+package com.example.mobileprojectapp2.activity.chutro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class YeuCauDatPhongChiTietActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_yeu_cau_dat_phong_chi_tiet);
+    }
+}
