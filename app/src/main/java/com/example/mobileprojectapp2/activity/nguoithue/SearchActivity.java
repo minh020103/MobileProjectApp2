8ea162930310ab2fa28df79b7ad64eeae0783680
phuc0707 <nguyenhphuc0707@gmail.com>
2023-12-06 00:50:27 +0700

    Update searchnguoithue v1

---
+++ app/src/main/java/com/example/mobileprojectapp2/activity/nguoithue/SearchActivity.java
@@ -0,0 +1,16 @@
+package com.example.mobileprojectapp2.activity.nguoithue;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mobileprojectapp2.R;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.nguoithue_search_activity);
+    }
+}
