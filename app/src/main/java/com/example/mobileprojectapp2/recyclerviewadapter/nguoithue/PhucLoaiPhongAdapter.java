df467ef9ec00d71c878cadd7c227e56c01d80aff
phuc0707 <nguyenhphuc0707@gmail.com>
2023-12-09 00:56:40 +0700

    Update search nguoithue v3.1

--- app/src/main/java/com/example/mobileprojectapp2/recyclerviewadapter/nguoithue/PhucLoaiPhongAdapter.java
+++ app/src/main/java/com/example/mobileprojectapp2/recyclerviewadapter/nguoithue/PhucLoaiPhongAdapter.java
@@ -1,62 +1,52 @@
 package com.example.mobileprojectapp2.recyclerviewadapter.nguoithue;
 
 import android.app.Activity;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.bumptech.glide.Glide;
 import com.example.mobileprojectapp2.R;
 import com.example.mobileprojectapp2.api.Const;
-import com.example.mobileprojectapp2.datamodel.Quan;
-import com.example.mobileprojectapp2.model.LoaiPhong;
 
 import java.util.List;
 
-public class LoaiPhongAdaprer extends RecyclerView.Adapter<LoaiPhongAdaprer.MyViewHolder> {
-    private Activity activity;
-    private List<LoaiPhong> list;
-    private int layoutID;
-    private OnClick onClick;
+public class LoaiPhongAdapter2 extends RecyclerView.Adapter<LoaiPhongAdapter2.MyViewHolder> {
 
-    public LoaiPhongAdaprer(Activity activity, List<LoaiPhong> list, int layoutID) {
-        this.activity = activity;
-        this.list = list;
-        this.layoutID = layoutID;
-    }
+    private Activity activity;
+    private int layoutID;
+    private List<Integer> list;
+    private OnClick onClick;
 
     public void setOnClick(OnClick onClick) {
         this.onClick = onClick;
     }
 
+    public LoaiPhongAdapter2(Activity activity, List<Integer> list, int layoutID) {
+        this.activity = activity;
+        this.layoutID = layoutID;
+        this.list = list;
+    }
+
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(layoutID, parent, false);
+        CardView view = (CardView) activity.getLayoutInflater().inflate(layoutID, parent, false);
         return new MyViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-        LoaiPhong loaiPhong = list.get(position);
-        if (loaiPhong != null){
-            holder.tvLoaiPhong.setText(loaiPhong.getLoaiPhong());
-        }
-
+        holder.tvLoaiPhong.setText(list.get(position) == Const.PHONG_TRONG? "Phòng trống":list.get(position) == Const.PHONG_DON?"Phòng đơn":"Phòng ghép");
         holder.onClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                onClick.onClickItemListenner(position, view);
+                onClick.onClickItemListener(position, view);
             }
         };
-
-
     }
 
     @Override
@@ -65,13 +55,12 @@ public class LoaiPhongAdaprer extends RecyclerView.Adapter<LoaiPhongAdaprer.MyVi
     }
 
     public interface OnClick{
-        void onClickItemListenner(int position, View view);
+        void onClickItemListener(int position, View view);
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         TextView tvLoaiPhong;
         View.OnClickListener onClickListener;
-
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             tvLoaiPhong = itemView.findViewById(R.id.tv_loai_phong);
