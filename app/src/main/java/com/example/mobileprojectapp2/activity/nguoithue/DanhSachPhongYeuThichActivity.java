23c0f5d9f031fc0ec5130037ddd6f930de95d753
phuc0707 <nguyenhphuc0707@gmail.com>
2023-12-12 00:09:47 +0700

    update danhsachphongyeuthich v1.1

---
+++ app/src/main/java/com/example/mobileprojectapp2/activity/nguoithue/DanhSachPhongYeuThichActivity.java
@@ -0,0 +1,16 @@
+package com.example.mobileprojectapp2.activity.nguoithue;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mobileprojectapp2.R;
+
+public class DanhSachPhongYeuThichActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_danh_sach_phong_yeu_thich);
+    }
+}
